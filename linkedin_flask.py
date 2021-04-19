32f6c302a71e013173f4e3a2a6d9237610a23711
Jamal Saied <jsaied99@gmail.com>
2021-04-18 21:06:09 -0500

    adding req

--- linkedin_flask.py
+++ linkedin_flask.py
@@ -5,10 +5,12 @@ from flask import request, jsonify
 from flask import render_template, redirect
 from linkedin import linkedin
 from urllib.request import urlopen
+import requests
+from lxml import html
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
+TOKEN = ""
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,13 +23,17 @@ def home():
         s = """https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s""" % (
             client_id, client_secret, code, redirect_uri)
         # return s
-        return redirect(s)
+        # page = requests.get(s)
+        TOKEN = requests.get(s).json()["access_token"]
+        application = linkedin.LinkedInApplication(token=TOKEN)
 
-        # application = linkedin.LinkedInApplication(token=token)
-        # return application.get_profile()
+
+        # linkedin_all()
+        # print(application.get_profile())
+        return application.get_profile()
+
     url = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78yuk1i7oafw90&scope=r_liteprofile&state=123456&redirect_uri=http://localhost:5000"
-    # return urllib.request.urlopen(url).read().decode()
-    return url
+    return redirect(url)
 
 
 @app.errorhandler(404)
@@ -37,10 +43,22 @@ def page_not_found(e):
 
 @app.route('/code', methods=['GET'])
 def linkedin_all():
-    if 'code' in request.args:
-        token = request.args['code']
-        application = linkedin.LinkedInApplication(token=token)
-    return "<h2>" + str(application.get_profile()) + "</h2>"
+    # code = request.args['code']
+    # client_id = "78yuk1i7oafw90"
+    # client_secret = "UHo4QTDPugd9BdRH"
+    # redirect_uri = "http://localhost:5000/code"
+
+    # s = """https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s""" % (
+    #     client_id, client_secret, code, redirect_uri)
+    # return data
+
+    # application = linkedin.LinkedInApplication(token=TOKEN)
+
+    # if 'code' in request.args:
+    #     token = request.args['code']
+    #     application = linkedin.LinkedInApplication(token=token)
+    # return str(application.get_profile())
+    return TOKEN
 
 
 app.run()
